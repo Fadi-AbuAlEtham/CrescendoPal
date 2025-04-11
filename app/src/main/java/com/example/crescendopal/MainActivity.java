670618036e97d46d538809ce278aa21a743c6522
Fadi Abu Aletham <fadiezam@hotmail.com>
2025-04-11 22:10:44 +0200

    feat: Implement splash screen with animations and new assets
    This commit introduces a splash screen for the application with visual enhancements and new features.
    
    -   Added `top_animation.xml` and `bottom_animation.xml` for animating the splash screen elements.
    -   Added a new string resource `"crescendo_pal"` to be displayed on the splash screen.
    -   Added new image asset `crescendo_pal_logo.png` and `crescendo_pal_logo_circ.png` to be displayed on the splash screen.
    -   Added new font resources `aclonica.xml`, `adlam_display.xml`, and `aguafina_script.xml`.
    -   Added a resource file `preloaded_fonts.xml` that defines preloaded fonts for the app.
    -   Added a resource file `font_certs.xml` that defines font certificates.
    -   Updated `activity_main.xml` to display the splash screen elements including an image and text with custom styling.
    -   Modified `MainActivity.java` to include animations and a timed transition to the Dashboard activity after 5 seconds.
    - Added empty Dashboard and MainActivity2 classes.
    -   Updated `AndroidManifest.xml` to define new activities, and the preloaded fonts.
    - Created `deploymentTargetSelector.xml`, `misc.xml` and `vcs.xml` files.

--- app/src/main/java/com/example/crescendopal/MainActivity.java
+++ app/src/main/java/com/example/crescendopal/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.crescendopal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Animation top, bottom;
+    private TextView txt;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        txt = findViewById(R.id.txt);
+        img = findViewById(R.id.img);
+
+        txt.setAnimation(bottom);
+        img.setAnimation(top);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
