548ad396a7ccf1d2d19aa8c85abfcd13f7651bd4
Fadi Abu Aletham <fadiezam@hotmail.com>
2025-04-14 22:21:57 +0300

    feat: Enhance Dashboard UI, add navigation, and update dependencies
    This commit enhances the Dashboard's user interface, introduces navigation to different sections of the app, and updates project dependencies.
    
    -   Updated `gradle/libs.versions.toml`:
        -   Added `gson` library dependency, version 2.10.1, to manage data.
        -   Added `gridlayout` library dependency, version 1.1.0, to use Grid layout in the app.
    -   Modified `app/src/main/res/values/strings.xml`:
        -   Added strings for cart, instruments, add instrument, description, price, seller, instrument type, condition, and rental option, upload image and checkout.
    -   Modified `app/src/main/res/values/preloaded_fonts.xml`:
        -   Added `abril_fatface`, `albert_sans_bold` and `alexandria_bold` fonts to the preloaded fonts.
    -   Modified `app/src/main/java/com/example/crescendopal/Dashboard.java`:
        -   Added navigation to the Instruments and Classes activities.
        -   Added `instrumentCard` and `classesCard`.
        -   Added click listeners to navigate to InstrumentsActivity and ClassesActivity.
    -   Created `app/src/main/res/layout/activity_dashboard.xml`:
        -   Added CardViews for "Instruments", "Lessons/Classes", "Books/Sheets", and "My Hub".
        -   Improved the layout for the dashboard by adding a logo, title, and footer text.
        -   Added app logo, app title and footer text.
        - Added `ic_instrument`, `ic_classes`, `ic_books` and `ic_hub` assets for the new CardViews.

--- app/src/main/java/com/example/crescendopal/Dashboard.java
+++ app/src/main/java/com/example/crescendopal/Dashboard.java
@@ -1,15 +1,18 @@
 package com.example.crescendopal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Dashboard extends AppCompatActivity {
-
+    CardView instrumentCard, classesCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,29 @@ public class Dashboard extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find the cards
+        instrumentCard = findViewById(R.id.instrumentCard);
+        classesCard = findViewById(R.id.classesCard);
+
+        // Set click listeners
+        instrumentCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to InstrumentsActivity
+                Intent intent = new Intent(Dashboard.this, InstrumentsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        classesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to ClassesActivity
+                Intent intent = new Intent(Dashboard.this, ClassesActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
